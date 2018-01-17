13cd796e8630cdc899d5ac122ada62c3ad72d8fb
claydergc <claydergc@gmail.com>
2018-01-16 22:34:07 -0300

    Util.h and Util.cpp

---
+++ Util.h
@@ -0,0 +1,16 @@
+// Util.h
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#define EPS 2.2204e-16
+
+using namespace std;
+
+void findPeaks(vector<float> x0, vector<int>& peakInds);
+
+#endif
